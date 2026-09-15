26246decb75846dc7976c3088a3271e21b1edcf1
Justin Chung <hi1214justin@gmail.com>
2026-09-15 09:36:04 +0800

    Initial Commit

---
+++ Week 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week 2
+//
+//  Created by 鍾心哲 on 2026/9/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week 2/Week_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Week_2App.swift
+//  Week 2
+//
+//  Created by 鍾心哲 on 2026/9/15.
+//
+
+import SwiftUI
+
+@main
+struct Week_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
